c5f9bbec9886adeb476c72da0ff05fbf404fcf8c
KL <kinglaugood@gmail.com>
2015-10-29 14:54:18 -0400

    15th

--- Test_01/src/testcode01.java
+++ Test_01/src/testcode01.java
@@ -3,7 +3,13 @@ public class testcode01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//test 
+		//test 
+		System.out.println(1<2? "true": "false");
+		int i = 0;
+		while(i<10){
+			System.out.print(i);
+			i++;
+		}
 	}
 
 }
