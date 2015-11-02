c8699fd6c66fdc84061db59e6465b7cc9badfca4
KL <kinglaugood@gmail.com>
2015-11-02 16:38:39 -0500

    16th

---
+++ Test_01/src/GradeBook.java
@@ -0,0 +1,33 @@
+
+public class GradeBook {
+	
+	private int[] grades;
+	
+	
+	
+	public GradeBook(){
+	
+	}
+	
+	public void getGrade(){
+		int i_length = grades.length;
+		
+		
+		for(int i = 1; i< i_length;i++){
+			
+		}
+	}
+	
+	public void setGrade(){
+		grades = new int[10];
+		
+	}
+	
+	public void calcGradeAvg(){
+		
+	}
+	
+	public void displayGrade(){
+		
+	}
+}
